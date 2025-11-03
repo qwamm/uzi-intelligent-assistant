49cf9ce238ce7b78d114855f2bd936efc5e99dcd
afonin.is <afonin.is@nppgamma.ru>
2025-11-03 16:03:17 +0300

    обеновление requirements.txt для medweb и новый nginx.conf

--- nginx/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export DJ_STATIC_FILES=/usr/src/web/static_files/front/
-cp ./manifest.json $DJ_STATIC_FILES
-cp -r ./openseadragon-images/ $DJ_STATIC_FILES
-cd static/
-cp -r ./css/ $DJ_STATIC_FILES
-cp -r ./js/ $DJ_STATIC_FILES
-exec "$@"
